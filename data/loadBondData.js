11b4fb2be86eaab9b739b370e63bf49ef1cce6c1
Dave Harned <dharned@gmail.com>
2017-02-19 14:04:39 -0600

    first commit

---
+++ data/loadBondData.js
@@ -0,0 +1,438 @@
+var MongoClient = require('mongodb').MongoClient;
+var dbName = 'bond';
+var local_mongo = 'mongodb://localhost:27017/' + dbName;
+
+MongoClient.connect(local_mongo, function(err, database) {
+    if (err) {
+        console.log('Error: ' + err);
+        process.exit(1);
+    }
+    console.log('successfully connected to Mongo database: ' + dbName);
+
+    database.collection('goldenAPI').insertMany(bondData, function(err) {
+        if (err) {
+            console.log('failed to insert data: ' + err.message);
+        } else {
+            console.log('insertMany succeeded!');
+        }
+    });
+
+    database.close();
+});
+
+var bondData = [
+    {
+        "title": "Dr. No",
+        "year": 1962,
+        "stars": [
+                "Sean Connery",
+                "Ursula Andress",
+                "Bernard Lee"
+        ],
+        "director": "Terence Young",
+        "imdb": "tt0055928",
+        "theme": {
+            "title": "James Bond Theme",
+            "artist": "John Barry & Orchestra"
+        },
+        "synopsis": "A resourceful British government agent seeks answers in a case involving the disappearance of a colleague and the disruption of the American space program."
+    },
+    {
+        "title": "From Russia With Love",
+        "year": 1963,
+        "stars": [
+                "Sean Connery",
+                "Robert Shaw",
+                "Lotte Lenya"
+        ],
+        "director": "Terence Young",
+        "imdb": "tt0057076",
+        "theme": {
+            "title": "From Russia With Love",
+            "artist": "Matt Monro"
+        },
+        "synopsis": "James Bond willingly falls into an assassination ploy involving a naive Russian beauty in order to retrieve a Soviet encryption device that was stolen by SPECTRE."
+    },
+    {
+        "title": "Goldfinger",
+        "year": 1964,
+        "stars": [
+                "Sean Connery",
+                "Gert Frobe",
+                "Honor Blackman"
+        ],
+        "director": "Guy Hamilton",
+        "imdb": "tt0058150",
+        "theme": {
+            "title": "Goldfinger",
+            "artist": "Shirley Bassey"
+        },
+        "synopsis": "Investigating a gold magnate's smuggling, James Bond uncovers a plot to contaminate the Fort Knox gold reserve."
+    },
+    {
+        "title": "Thunderball",
+        "year": 1965,
+        "stars": [
+                "Sean Connery",
+                "Claudine Auger",
+                "Adolfo Celi"
+        ],
+        "director": "Terence Young",
+        "imdb": "tt0059800",
+        "theme": {
+            "title": "Thunderball",
+            "artist": "Tom Jones"
+        },
+        "synopsis": "James Bond heads to The Bahamas to recover two nuclear warheads stolen by SPECTRE agent Emilio Largo in an international extortion scheme."
+    },
+    {
+        "title": "You Only Live Twice",
+        "year": 1967,
+        "stars": [
+                "Sean Connery",
+                "Akiko Wakabayashi",
+                "Mie Hama"
+        ],
+        "director": "Lewis Gilbert",
+        "imdb": "tt0062512",
+        "theme": {
+            "title": "You Only Live Twice",
+            "artist": "Nancy Sinatra"
+        },
+        "synopsis": "Agent 007 and the Japanese secret service ninja force must find and stop the true culprit of a series of spacejackings before nuclear war is provoked."
+    },
+    {
+        "title": "Casino Royale",
+        "year": 1967,
+        "stars": [
+                "David Niven",
+                "Peter Sellers",
+                "Ursula Andress"
+        ],
+        "director": "Ken Hughes",
+        "imdb": "tt0061452",
+        "theme": {
+            "title": "Casino Royale",
+            "artist": "Herb Alpert & the Tijuana Brass"
+        },
+        "synopsis": "In an early spy spoof, aging Sir James Bond comes out of retirement to take on SMERSH."
+    },    
+    {
+        "title": "On Her Majesty’s Secret Service",
+        "year": 1969,
+        "stars": [
+                "George Lazenby",
+                "Diana Rigg",
+                "Telly Savalas"
+        ],
+        "director": "Peter R. Hunt",
+        "imdb": "tt0064757",
+        "theme": {
+            "title": "We Have All The Time In The World",
+            "artist": "Louis Armstrong"
+        },
+        "synopsis": "James Bond woos a mob boss's daughter and goes undercover to uncover the true reason for Blofeld's allergy research in the Swiss Alps that involves beautiful women from around the world."
+    },
+    {
+        "title": "Diamonds Are Forever",
+        "year": 1971,
+        "stars": [
+                "Sean Connery",
+                "Jill St. John",
+                "Charles Gray"
+        ],
+        "director": "Guy Hamilton",
+        "imdb": "tt0066995",
+        "theme": {
+            "title": "Diamonds Are Forever",
+            "artist": "Shirley Bassey"
+        },
+        "synopsis": "A diamond smuggling investigation leads James Bond to Las Vegas, where he uncovers an evil plot involving a rich business tycoon."
+    },
+    {
+        "title": "Live and Let Die",
+        "year": 1973,
+        "stars": [
+                "Roger Moore",
+                "Yaphet Kotto",
+                "Jane Seymour"
+        ],
+        "director": "Guy Hamilton",
+        "imdb": "tt070328",
+        "theme": {
+            "title": "Live And Let Die",
+            "artist": "Paul McCartney & Wings"
+        },
+        "synopsis": "007 is sent to stop a diabolically brilliant heroin magnate armed with a complex organization and a reliable psychic tarot card reader."
+    },
+    {
+        "title": "The Man with the Golden Gun",
+        "year": 1974,
+        "stars": [
+                "Roger Moore",
+                "Christopher Lee",
+                "Britt Ekland"
+        ],
+        "director": "Guy Hamilton",
+        "imdb": "tt0071807",
+        "theme": {
+            "title": "The Man With The Golden Gun",
+            "artist": "Lulu"
+        },
+        "synopsis": "James Bond is led to believe that he is targeted by the world's most expensive assassin while he attempts to recover sensitive solar cell technology that is being sold to the highest bidder."
+    },
+    {
+        "title": "The Spy Who Loved Me",
+        "year": 1977,
+        "stars": [
+                "Roger Moore"
+        ],
+        "director": "Lewis Gilbert",
+        "imdb": "tt0076752",
+        "theme": {
+            "title": "Nobody Does It Better",
+            "artist": "Carly Simon"
+        },
+        "synopsis": "James Bond investigates the hijacking of British and Russian submarines carrying nuclear warheads with the help of a KGB agent whose lover he killed."
+    },
+    {
+        "title": "Moonraker",
+        "year": 1979,
+        "stars": [
+                "Roger Moore",
+                "Lois Chiles",
+                "Micheal Lonsdale"
+        ],
+        "director": "Lewis Gilbert",
+        "imdb": "tt079574",
+        "theme": {
+            "title": "Moonraker",
+            "artist": "Shirley Bassey"
+        },
+        "synopsis": "James Bond investigates the mid-air theft of a space shuttle and discovers a plot to commit global genocide."
+    },
+    {
+        "title": "For Your Eyes Only",
+        "year": 1981,
+        "stars": [
+                "Roger Moore",
+                "Carole Bouquet",
+                "Topol"
+        ],
+        "director": "John Glen",
+        "imdb": "tt082398",
+        "theme": {
+            "title": "For Your Eyes Only",
+            "artist": "Sheena Easton"
+        },
+        "synopsis": "Agent 007 is assigned to hunt for a lost British encryption device and prevent it from falling into enemy hands."
+    },
+    {
+        "title": "Octopussy",
+        "year": 1983,
+        "stars": [
+                "Roger Moore",
+                "Maud Adams",
+                "Louis Jordan"
+        ],
+        "director": "John Glen",
+        "imdb": "tt0086034",
+        "theme": {
+            "title": "All Time High",
+            "artist": "Rita Coolidge"
+        },
+        "synopsis": "A fake Fabergé egg and a fellow agent's death lead James Bond to uncover an international jewel-smuggling operation, headed by the mysterious Octopussy, being used to disguise a nuclear attack on N.A.T.O. forces."
+    },
+    {
+        "title": "Never Say Never Again",
+        "year": 1983,
+        "stars": [
+                "Sean Connery",
+                "Kim Basinger",
+                "Klaus Maria Brandauer"
+        ],
+        "director": "Irvin Kershner",
+        "imdb": "tt0086006",
+        "theme": {
+            "title": "Never Say Never Again",
+            "artist": "Lani Hall"
+        },
+        "synopsis": "A SPECTRE agent has stolen two American nuclear warheads, and James Bond must find their targets before they are detonated."
+    },    
+    {
+        "title": "A View to a Kill",
+        "year": 1985,
+        "stars": [
+                "Roger Moore",
+                "Christopher Walken",
+                "Tanya Roberts"
+        ],
+        "director": "John Glen",
+        "imdb": "tt0090264",
+        "theme": {
+            "title": "A View To A Kill",
+            "artist": "Duran Duran"
+        },
+        "synopsis": "An investigation of a horse-racing scam leads 007 to a mad industrialist who plans to create a worldwide microchip monopoly by destroying California's Silicon Valley."
+    },
+    {
+        "title": "The Living Daylights",
+        "year": 1987,
+        "stars": [
+                "Timothy Dalton",
+                "Maryam d'Abo",
+                "Jeroen Krabbe"
+        ],
+        "director": "John Glen",
+        "imdb": "tt0093428",
+        "theme": {
+            "title": "The Living Daylights",
+            "artist": "a-ha"
+        },
+        "synopsis": "James Bond is living on the edge to stop an evil arms dealer from starting another world war. Bond crosses all seven continents in order to stop the evil Whitaker and General Koskov."
+    },
+    {
+        "title": "License to Kill",
+        "year": 1989,
+        "stars": [
+                "Timothy Dalton",
+                "Robert Davi",
+                "Carey Lowell"
+        ],
+        "director": "John Glen",
+        "imdb": "tt0097742",
+        "theme": {
+            "title": "Licence To Kill",
+            "artist": "Gladys Knight"
+        },
+        "synopsis": "James Bond goes rogue and sets off to unleash vengeance on a drug lord who tortured his best friend, a C.I.A. agent, and left him for dead and murdered his bride after he helped capture him."
+    },
+    {
+        "title": "Goldeneye",
+        "year": 1995,
+        "stars": [
+                "Pierce Brosnan",
+                "Sean Bean",
+                "Izabella Scorupco"
+        ],
+        "director": "Martin Campbell",
+        "imdb": "tt0113189",
+        "theme": {
+            "title": "GoldenEye",
+            "artist": "Tina Turner"
+        },
+        "synopsis": "James Bond teams up with the lone survivor of a destroyed Russian research center to stop the hijacking of a nuclear space weapon by a fellow agent formerly believed to be dead."
+    },
+    {
+        "title": "Tomorrow Never Dies",
+        "year": 1997,
+        "stars": [
+                "Pierce Brosnan",
+                "Jonathan Price",
+                "Michelle Yeoh"
+        ],
+        "director": "Roger Spottiswoode",
+        "imdb": "tt0120347",
+        "theme": {
+            "title": "Tomorrow Never Dies",
+            "artist": "Sheryl Crow"
+        },
+        "synopsis": "James Bond heads to stop a media mogul's plan to induce war between China and the UK in order to obtain exclusive global media coverage."
+    },
+    {
+        "title": "The World Is Not Enough",
+        "year": 1999,
+        "stars": [
+                "Pierce Brosnan",
+                "Sophie Marceau",
+                "Robert Carlyle"
+        ],
+        "director": "Michael Apted",
+        "imdb": "tt0143145",
+        "theme": {
+            "title": "The World Is Not Enough",
+            "artist": "Garbage"
+        },
+        "synopsis": "James Bond uncovers a nuclear plot when he protects an oil heiress from her former kidnapper, an international terrorist who can't feel pain."
+    },
+    {
+        "title": "Die Another Day",
+        "year": 2002,
+        "stars": [
+                "Pierce Brosnan",
+                "Halle Berry",
+                "Rosamund Pike"
+        ],
+        "director": "Lee Tamahori",
+        "imdb": "tt0246460",
+        "theme": {
+            "title": "Die Another Day",
+            "artist": "Madonna"
+        },
+        "synopsis": "James Bond is sent to investigate the connection between a North Korean terrorist and a diamond mogul who is funding the development of an international space weapon."
+    },
+    {
+        "title": "Casino Royale",
+        "year": 2006,
+        "stars": [
+                "Daniel Craig",
+                "Eva Green",
+                "Judi Dench"
+        ],
+        "director": "Martin Campbell",
+        "imdb": "tt0381061",
+        "theme": {
+            "title": "You Know My Name",
+            "artist": "Chris Cornell"
+        },
+        "synopsis": "Armed with a licence to kill, Secret Agent James Bond sets out on his first mission as 007 and must defeat a weapons dealer in a high stakes game of poker at Casino Royale, but things are not what they seem."
+    },
+    {
+        "title": "Quantum of Solace",
+        "year": 2008,
+        "stars": [
+                "Daniel Craig",
+                "Olga Kurylenka",
+                "Mathieu Amalric"
+        ],
+        "director": "Marc Forster",
+        "imdb": "tt0830515",
+        "theme": {
+            "title": "Another Way To Die",
+            "artist": "Jack White & Alicia Keys"
+        },
+        "synopsis": "James Bond descends into mystery as he tries to stop a mysterious organization from eliminating a country's most valuable resource. All the while, he still tries to seek revenge over the death of his love."
+    },
+    {
+        "title": "Skyfall",
+        "year": 2012,
+        "stars": [
+                "Daniel Craig",
+                "Javier Bardem",
+                "Naomie Harris"
+        ],
+        "director": "Sam Mendes",
+        "imdb": "tt1074638",
+        "theme": {
+            "title": "Skyfall",
+            "artist": "Adele"
+        },
+        "synopsis": "Bond's loyalty to M is tested when her past comes back to haunt her. Whilst MI6 comes under attack, 007 must track down and destroy the threat, no matter how personal the cost."
+    },
+    {
+        "title": "Spectre",
+        "year": 2015,
+        "stars": [
+                "Daniel Craig",
+                "Christoph Waltz",
+                "Lea Seydoux"
+        ],
+        "director": "Sam Mendes",
+        "imdb": "tt2379713",
+        "theme": {
+            "title": "Writing's On The Wall",
+            "artist": "Sam Smith"
+        },
+        "synopsis": "A cryptic message from Bond's past sends him on a trail to uncover a sinister organization. While M battles political forces to keep the secret service alive, Bond peels back the layers of deceit to reveal the terrible truth behind SPECTRE."
+    }
+];
---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+
+var dbName = 'bond';
+var local_mongo = 'mongodb://localhost:27017/' + dbName;
+//var heroku_mongo = process.env.MONGODB_URI;
+
+// Express server setup
+var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+// Initiate database connection
+MongoClient.connect(local_mongo, function(err, database){
+    if (err) {
+        console.log('Error: ' + err);
+        process.exit(1);
+    }
+    console.log('successfully connected to Mongo database: ' + dbName);
+
+    // Initialize the app.
+    app.listen(3000, function () {
+        console.log("App now running on port 3000");
+    });
+
+    // Handle Get request to root
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname + '/index.html'));
+    })
+
+    // Handle GET request /bondfilms: get all film data
+    app.get("/bondfilms", function(req, res) {
+        database.collection('goldenAPI').find({}).toArray(function(err, docs) {
+            if (err) {
+                console.log('failed to get data: ' + err.message);
+            } else {
+                res.status(200).json(docs);
+            }
+        });
+    });
+
+
+});
